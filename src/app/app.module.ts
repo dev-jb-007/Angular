5ad7481f0bc4bd6a887a8913c7059f9a76d8b688
Dev Patel <devpatel8907@gmail.com>
2021-08-20 22:24:16 +0530

    Creating Custom Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { ServerComponent } from "./server/server.component";
+import { FooterComponent} from "./Footer/footer.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
