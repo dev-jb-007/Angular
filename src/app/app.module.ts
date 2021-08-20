95d997107978389151a4b3d2328f9ef9fae11f28
Dev Patel <devpatel8907@gmail.com>
2021-08-20 23:19:35 +0530

    Creating and customising custom components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ServerComponent } from "./server/server.component";
 import { FooterComponent} from "./Footer/footer.component";
+import { ServersComponent } from './servers/servers.component';
+import { SuccessComponent} from "./warnings/success/success.component";
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
-    FooterComponent
+    FooterComponent,
+    ServersComponent,
+    SuccessComponent
   ],
   imports: [
     BrowserModule,
