493fdb6e06abe06dda78f57a799b796766620ea7
Dev Patel <devpatel8907@gmail.com>
2021-08-21 13:50:16 +0530

    Property Biding and string manipulation

---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  // selector: '[app-servers]', we can use this to and then add attribute to html elements in template
+  // selector:'.app-servers' we can also use class selector and add this class in html elements in template
+  selector: 'app-servers',
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent implements OnInit {
+  showserver: string = 'none';
+  showbutton:boolean=true;
+  name:string='Dev Manish Patel'
+  showServer(){
+    if(this.showserver ==='block')
+    {
+      this.showserver='none';
+    }
+    else{
+      this.showserver='block';
+    }
+
+  }
+  constructor() {
+    setTimeout(()=>{
+      this.showbutton=false;
+    },2000)
+  }
+
+  ngOnInit(): void {
+  }
+
+}
